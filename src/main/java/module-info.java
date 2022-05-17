7812fd74aa8325ccd64fb486ce78ed4b3a54848a
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2022-05-17 22:47:12 +0200

    import initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module sae201.projet {
+    requires transitive javafx.base;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
+    requires transitive javafx.fxml;
+
+    requires jakarta.persistence;
+
+    exports fr.univ_amu.iut;
+}
